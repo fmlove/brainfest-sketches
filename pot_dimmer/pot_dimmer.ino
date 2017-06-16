0816c13b659afbffd8135d275f6be358a75e7229
fmlove <fminl@comcast.net>
2017-06-16 16:58:16 +0100

    Removing unnecessary comment

--- pot_dimmer/pot_dimmer.ino
+++ pot_dimmer/pot_dimmer.ino
@@ -2,6 +2,9 @@ int sensorPin = A3;    // The potentiometer is connected to A3
 int ledPin = 0;      // The LED is connected to pin 0
 int outPin = 1;
 
+int sensorValue;
+int outValue;
+
 void setup() // this function runs once when the sketch starts up
 {
   pinMode(ledPin, OUTPUT);
@@ -9,14 +12,11 @@ void setup() // this function runs once when the sketch starts up
 
 void loop() // this function runs repeatedly after setup() finishes
 {
-  int sensorValue;
-  int outValue;
   
-  sensorValue = analogRead(sensorPin);    
+  sensorValue = analogRead(sensorPin);
+
   outValue = map(sensorValue, 0, 1023, 254, 0);//reversed for intuitiveness
   analogWrite(ledPin, outValue);
   analogWrite(outPin, outValue);
-  /*digitalWrite(outPin, HIGH);
-  delay(map(sensorValue, 0, 1023, 250, 1));
-  digitalWrite(outPin, LOW);*/
+
 }
